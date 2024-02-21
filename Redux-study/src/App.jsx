b9d2b2088f6fa756189e0513b78b75cf877b1289
mufeednm <mufeedmusthafanm@gmail.com>
2024-02-21 13:24:07 +0530

    just make task

---
+++ Redux-study/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import Main from './components/Main'
+import Store from './STORE/Store'
+function App() {
+  return (
+    <div >
+      <Main />
+    </div>
+  )
+}
+
+export default App
