fcd9f76fb52cb4a48982b51a584eb7ab464ab835
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-15 20:28:02 +0530

    just provide a store in main jsxr

--- redux in  admin  page/src/main.jsx
+++ redux in  admin  page/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./index.css"
-
+import { Provider } from 'react'
+import { store } from './Admin/store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
+<Provider store={store} >
 
     <App />
+</Provider>
   </BrowserRouter>
   </React.StrictMode>,
 )
