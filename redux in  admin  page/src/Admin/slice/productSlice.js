d580b01ef654679bb781c91184e6e05eb5538c12
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-16 15:39:27 +0530

    store has an error corrtected

--- redux in  admin  page/src/Admin/slice/productSlice.js
+++ redux in  admin  page/src/Admin/slice/productSlice.js
@@ -2,6 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 import { DummyProducts } from "../../pages/data";
 const productSlice = createSlice({
     name : "Products",
-    initialState : {value :DummyProducts}
+    initialState : {value :DummyProducts},
+    reducers: {
+        products:(state,action)=>{
+            state.value=action.payload
+        }
+    }
 }
 ) 
+export const {products}=productSlice.actions
+export default productSlice.reducer
--- redux in  admin  page/src/main.jsx
+++ redux in  admin  page/src/main.jsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./index.css"
-import { Provider } from 'react'
+
 import { store } from './Admin/store/store.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
+
   <BrowserRouter>
-<Provider store={store} >
+
 
     <App />
-</Provider>
+
   </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
