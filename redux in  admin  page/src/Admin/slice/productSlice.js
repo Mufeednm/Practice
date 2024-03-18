e489abcd58bab9c725b06f31ba6395569bab3b89
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-18 16:11:19 +0530

    handling errors

--- redux in  admin  page/src/Admin/slice/productSlice.js
+++ redux in  admin  page/src/Admin/slice/productSlice.js
@@ -1,17 +1,17 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { DummyProducts } from "../../pages/data";
-// const productSlice = createSlice({
-//     name : "Products",
-//     initialState : {value :DummyProducts},
-//     reducers: {
-//         productedit:(state,action)=>{
-//             state.value=action.payload
-//         },
-//         productremove:(state,action)=>{
-//              state.value.filter((value)=>value.id!==action.payload)
-//         }
-//     }
-// }
-// ) 
-// export const {productedit,productremove}=productSlice.actions
-// export default productSlice.reducer
+import { createSlice } from "@reduxjs/toolkit";
+import { DummyProducts } from "../../pages/data";
+const productSlice = createSlice({
+    name : "Products",
+    initialState : {value :DummyProducts},
+    reducers: {
+        productedit:(state,action)=>{
+            state.value=action.payload
+        },
+        productremove:(state,action)=>{
+             state.value.filter((value)=>value.id!==action.payload)
+        }
+    }
+}
+) 
+export const {productedit,productremove}=productSlice.actions
+export default productSlice.reducer
