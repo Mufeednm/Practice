0c237541179055a51f8c0b6c155fab64638602be
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-23 16:24:59 +0530

    errosss clear

--- redux in  admin  page/src/Admin/store/store.js
+++ redux in  admin  page/src/Admin/store/store.js
@@ -1,7 +1,7 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import productSlice from "../slice/productSlice";
-// export const store=configureStore({
-//     reducer : {
-//         product : productSlice
-//     }
-// })
+import { configureStore } from "@reduxjs/toolkit";
+import productSlice from "../slice/productSlice";
+export const store=configureStore({
+    reducer : {
+        product : productSlice
+    }
+})
