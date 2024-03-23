14df472ccf5ce6735ee934b10be5494ecd8cfe19
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-23 16:36:23 +0530

    async

--- javaScript/Promise/promise.js
+++ javaScript/Promise/promise.js
@@ -21,4 +21,45 @@ let myPromise = new Promise((resolve, reject) => {
       // If promise is rejected
       console.log("Error:", errorMessage);
     });
+
+    // Function to fetch data from an API
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    // Creating a new XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+
+    // Setting up the request
+    xhr.open('GET', url, true);
+
+    // Event handler for successful request
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        // Resolve the promise with the response text
+        resolve(xhr.responseText);
+      } else {
+        // Reject the promise with an error message
+        reject('Request failed with status ' + xhr.status);
+      }
+    };
+
+    // Event handler for failed request
+    xhr.onerror = function () {
+      // Reject the promise with an error message
+      reject('Request failed');
+    };
+
+    // Sending the request
+    xhr.send();
+  });
+}
+
+// Example usage of fetchData function
+fetchData('https://jsonplaceholder.typicode.com/posts/1')
+  .then((data) => {
+    console.log('Data fetched successfully:', data);
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
+
   
