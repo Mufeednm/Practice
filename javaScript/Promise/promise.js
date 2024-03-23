5b8c98eb1da45e720c5386e242e37af9bef8e4bb
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-23 16:33:24 +0530

    promise

--- javaScript/Promise/promise.js
+++ javaScript/Promise/promise.js
@@ -1,1 +1,24 @@
-console.log("jnhjchnbdsjn");
+// Creating a promise
+let myPromise = new Promise((resolve, reject) => {
+    // Asynchronous operation, e.g., fetching data from an API
+    setTimeout(() => {
+      let success = true; // Simulating success or failure
+      if (success) {
+        resolve("Operation successful"); // If successful, resolve the promise
+      } else {
+        reject("Operation failed"); // If failed, reject the promise
+      }
+    }, 2000); // Simulating a delay of 2 seconds
+  });
+  
+  // Consuming the promise
+  myPromise
+    .then((successMessage) => {
+      // If promise is fulfilled
+      console.log("Success:", successMessage);
+    })
+    .catch((errorMessage) => {
+      // If promise is rejected
+      console.log("Error:", errorMessage);
+    });
+  
