c0438bcea4ff824695e65b4bfd4d3672136cb1f1
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-28 17:13:12 +0530

    sample of promise 2

--- sample/promise.js
+++ sample/promise.js
@@ -1,10 +1,34 @@
-function abu() {
-    return new Promise ((resolve,reject)=>{
-     let x = setTimeout(()=>{
-            console.log("hello");
-        },1000)
-        resolve("babbuuuu")
+// function abu() {
+//     return new Promise ((resolve,reject)=>{
+//      let x = setTimeout(()=>{
+//             console.log("hello");
+//         },1000)
+//         resolve("babbuuuu")
+//     })
+// }
+// abu ()
+// .then((ans)=>{console.log(ans);}) 
+    //    another method 
+
+    // Creating a promise
+const myPromise = new Promise((resolve, reject) => {
+    
+    setTimeout(() => {
+      const success = true; // Simulating success
+      if (success) {
+        resolve("Operation completed successfully!"); // Resolve the promise
+      } else {
+        reject(new Error("Operation failed!")); // Reject the promise with an error
+      }
+    }, 2000); // Simulating a delay of 2 seconds
+  });
+  
+  // Using the promise
+  myPromise
+    .then((result) => {
+      console.log(result); // Output: Operation completed successfully!
     })
-}
-abu ()
-.then((ans)=>{console.log(ans);}) 
+    .catch((error) => {
+      console.error(error); // Output: Error: Operation failed!
+    });
+  
