572445af735e61f045cc7b92d95e48abbf252b61
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-30 10:58:03 +0530

    dom study class s

--- javaScript/Dom/js.js
+++ javaScript/Dom/js.js
@@ -2,3 +2,9 @@ let heading = document.getElementById("head");
  heading.innerHTML = "hdone"
  heading.style.backgroundColor = "green"
 console.log(heading);
+
+
+let classhead = document.getElementsByClassName("clase");
+classhead[0].innerHTML= "found class"
+classhead[0].style.backgroundColor="red"
+console.log(classhead[0]);
