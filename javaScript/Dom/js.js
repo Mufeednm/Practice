fd8846cd41866339cdafd505b01075e1e03738cd
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-30 20:26:54 +0530

    dom study delete

--- javaScript/Dom/js.js
+++ javaScript/Dom/js.js
@@ -1,10 +1,22 @@
 let heading = document.getElementById("head");
  heading.innerHTML = "hdone"
  heading.style.backgroundColor = "green"
-console.log(heading);
+// console.log(heading);
 
 
 let classhead = document.getElementsByClassName("clase");
 classhead[0].innerHTML= "found class"
 classhead[0].style.backgroundColor="red"
-console.log(classhead[0]);
+// console.log(classhead[0]);
+
+
+let tagname= document.getElementsByTagName("button")
+// console.log(tagname[0]);
+ 
+// delete an element in html 
+let delet = document.querySelector(".partb");
+delet.remove()
+console.log(delet);
+
+ 
+ 
