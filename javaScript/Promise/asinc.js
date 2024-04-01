efc620c92ddf9f59c214efa26098bdf714b712c5
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-01 13:52:57 +0530

    just arrange folders

--- javaScript/Promise/asinc.js
+++ javaScript/Promise/asinc.js
@@ -1,28 +1,32 @@
-// Asynchronous function to simulate fetching data
-async function fetchData() {
-  // Simulating an asynchronous operation, e.g., fetching data from an API
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let success = true; // Simulating success or failure
-      if (success) {
-        resolve("Data fetched successfully"); // Resolve the promise with data
-      } else {
-        reject("Error fetching data"); // Reject the promise with an error
-      }
-    }, 2000); // Simulating a delay of 2 seconds
-  });
-}
+// // Asynchronous function to simulate fetching data
+// async function fetchData() {
+//   // Simulating an asynchronous operation, e.g., fetching data from an API
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let success = true; // Simulating success or failure
+//       if (success) {
+//         resolve("Data fetched successfully"); // Resolve the promise with data
+//       } else {
+//         reject("Error fetching data"); // Reject the promise with an error
+//       }
+//     }, 2000); // Simulating a delay of 2 seconds
+//   });
+// }
 
-// Using the async function with await
-async function fetchDataAsync() {
-  try {
-    // Waiting for the fetchData function to complete
-    const result = await fetchData();
-    console.log(result); // Output the result
-  } catch (error) {
-    console.error(error); // Output any errors
-  }
-}
+// // Using the async function with await
+// async function fetchDataAsync() {
+//   try {
+//     // Waiting for the fetchData function to complete
+//     const result = await fetchData();
+//     console.log(result); // Output the result
+//   } catch (error) {
+//     console.error(error); // Output any errors
+//   }
+// }
 
-// Calling the async function
-fetchDataAsync();
+// // Calling the async function
+// fetchDataAsync();
+
+async function (params) {
+  
+}
---
+++ javaScript/Promise/idstactaring.js
@@ -0,0 +1,8 @@
+let x = {
+    name : "abu",
+    age : 22 ,
+    place : 122,
+};
+
+let {name:ands,age,place}= x
+console.log(ands);
