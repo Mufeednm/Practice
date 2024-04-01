51ca69f64830e237c089d69514d4bdc24c8ce66f
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-01 17:17:34 +0530

    use effect

--- react/react hooks/src/App.jsx
+++ react/react hooks/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState ,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [counts, setCounts] = useState(0)
+   useEffect(()=>{
+    console.log("manu");
+    
+      console.log("byeee")
+   },[count])
 
   return (
     <div>
-      
+      <button onClick={()=>setCount(count+1)} >{count}</button>
+      <button onClick={()=>setCounts(counts+1)} >{counts}</button>
     </div>
      
   )
