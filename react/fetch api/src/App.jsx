cbeee4b2b5b7d56b8fcc11f192731afd74a92dcf
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-04 16:02:29 +0530

    await studyn

--- react/fetch api/src/App.jsx
+++ react/fetch api/src/App.jsx
@@ -1,24 +1,57 @@
+// import { useState ,useEffect} from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [comming,setcomming]=useState([])
+// useEffect (()=>{
+// let geting= async ()=>{
+//   try {
+//     let datas = await fetch ("https://api.postalpincode.in/pincode/110001")
+//     let change =  await  datas.json()
+//     setcomming (change[0].PostOffice)
+    
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// geting();
+// {console.log(comming)}
+// },[])
+// return (
+//   <div>
+
+//    <h2>dsad</h2>
+//   </div>
+//   )
+// }
+
+
+// export default App
+
 import { useState ,useEffect} from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [get,setgets]=useState([])
-useEffect(()=>{
+  useEffect(()=>{
 
   let posts =async ()=>{
    let datas= await fetch ("https://api.postalpincode.in/pincode/110001")
    let final = await datas.json()
   //  setgets(final)
-  //  console.log(get);
   setgets(final[0].PostOffice)
-
-// console.log( "first",  final);
+  
+  // console.log( "first",  final);
 }
 posts()
 },[])
 console.log(get);
+// console.log(get);
 
   return (
     
